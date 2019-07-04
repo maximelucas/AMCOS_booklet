0b09e288978581751c285c3341bb297945d89591
Maxime Lucas <m.lucas@lancaster.ac.uk>
2019-07-04 13:44:29 +0200

    fixed bugged due to main->MAIN non consistently done

--- compile.sh
+++ compile.sh
@@ -4,14 +4,14 @@
 # todo: check if can be made cleaner with latexmk
 
 # compile for online version
-pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinetrue \input{main.tex}"
-pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinetrue \input{main.tex}"
-mv main.pdf booklet_online.pdf
+pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinetrue \input{MAIN.tex}"
+pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinetrue \input{MAIN.tex}"
+mv MAIN.pdf booklet_online.pdf
 
 # compile for printed version
-pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinefalse \input{main.tex}"
-pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinefalse \input{main.tex}"
+pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinefalse \input{MAIN.tex}"
+pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinefalse \input{MAIN.tex}"
 # extract timetable pages
 #pdftk main.pdf cat 5-9 output timetable.pdf
 #pdftk main.pdf cat 32-36 output list_posters.pdf
-mv main.pdf booklet_printed.pdf
+mv MAIN.pdf booklet_printed.pdf
